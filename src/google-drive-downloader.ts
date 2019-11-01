40ac6524bf0f0dfee2a91a91c7b762ba80246a10
Michael Gates <michaelgatesdev@gmail.com>
2019-11-01 12:57:18 -0400

    Add option to replace file

--- src/google-drive-downloader.ts
+++ src/google-drive-downloader.ts
@@ -3,22 +3,23 @@ import { StringUtils } from "@michaelgatesdev/common";
 import { WebDownloader } from "./downloader";
 import { FileUtils } from "./file-utils";
 
-
 export class GoogleDriveDownloader {
 
     public static BASE_URL = "https://docs.google.com/spreadsheets/d/{docID}/export?format={format}";
     // private static SHEET_URL: string = "https://docs.google.com/spreadsheets/d/{docID}/export?format={format}&gid={sheetID}";
 
-    public static async downloadSpreadsheet(docID: string, format: string, destination: string): Promise<boolean> {
+    public static async downloadSpreadsheet(docID: string, format = "xlsx", destination: string, forceReplace = true): Promise<boolean> {
         if (StringUtils.isBlank(docID)) {
             throw new Error("The docID of the spreadsheet must be specified");
         }
-        if (StringUtils.isBlank(format)) { format = "xlsx"; }
+        if (StringUtils.isBlank(format)) {
+            throw new Error("The format of the download must be specified");
+        }
         if (StringUtils.isBlank(destination)) {
             throw new Error("The destination of the spreadsheet must be specified");
         }
 
-        if (await FileUtils.checkExists(destination)) { return false; }
+        if (await FileUtils.checkExists(destination) && !forceReplace) { throw new Error("There is already a file at that location."); }
 
         const downloadURL = GoogleDriveDownloader.BASE_URL
             .replace("{docID}", docID)
